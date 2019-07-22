ca327d9ab4fc02777ffedb8dab70c2527f3f5af9
Dat Bui <datbnh@gmail.com>
2019-07-22 19:52:53 +1000

    + Implemented alpha mask for clipping with anti-alias.

--- KVH Kaleidoscope/KaleidoscopeTemplateExtractor.cs
+++ KVH Kaleidoscope/KaleidoscopeTemplateExtractor.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,13 +15,15 @@ namespace Kvh.Kaleidoscope
         public SmoothingMode SmoothingMode;
         public PixelOffsetMode PixelOffsetMode;
         public InterpolationMode InterpolationMode;
+        public bool UseAlphaBlend;
 
         public KaleidoscopeTemplateExtractor(SmoothingMode smoothingMode,
-            PixelOffsetMode pixelOffsetMode, InterpolationMode interpolationMode)
+            PixelOffsetMode pixelOffsetMode, InterpolationMode interpolationMode, bool useAlphaBlend = true)
         {
             SmoothingMode = smoothingMode;
             PixelOffsetMode = pixelOffsetMode;
             InterpolationMode = interpolationMode;
+            UseAlphaBlend = useAlphaBlend;
         }
 
         public KaleidoscopeTemplate Extract(Bitmap originalImage, 
@@ -45,11 +48,45 @@ namespace Kvh.Kaleidoscope
             gPattern.PixelOffsetMode = PixelOffsetMode;
             gPattern.InterpolationMode = InterpolationMode;
 
-            gPattern.Clip = new Region(clippingPath);
-            gPattern.RotateTransform(-angle);
-            gPattern.TranslateTransform(-x, -y);
-            gPattern.DrawImage(originalImage, scaledImgRect, imgRect, GraphicsUnit.Pixel);
-            gPattern.Dispose();
+
+            if (UseAlphaBlend)
+            {
+                var overlapping = 0;
+                var maskWidth = templateSize + 2* overlapping;
+                var maskHeight = maskWidth * (float)Math.Sqrt(3) / 2;
+
+                var maskPath = new GraphicsPath();
+                maskPath.AddPolygon(new[] {
+                new PointF(-overlapping, -overlapping*(float)Math.Sqrt(3)/2),
+                new PointF(maskWidth, -overlapping*(float)Math.Sqrt(3)/2),
+                new PointF(maskWidth/2, maskHeight)});
+
+
+                var alphaMask = new Bitmap(patternWidth, (int)(Math.Round(patternHeight)));
+                var gMask = Graphics.FromImage(alphaMask);
+                gMask.SmoothingMode = SmoothingMode;
+                gMask.PixelOffsetMode = PixelOffsetMode;
+                gMask.InterpolationMode = InterpolationMode;
+                gMask.Clear(Color.White);
+                gMask.FillPath(Brushes.Black, maskPath);
+                gMask.Dispose();
+
+                gPattern.RotateTransform(-angle);
+                gPattern.TranslateTransform(-x, -y);
+                gPattern.DrawImage(originalImage, scaledImgRect, imgRect, GraphicsUnit.Pixel);
+                gPattern.Dispose();
+
+                ApplyAlphaMask(pattern, alphaMask);
+            }
+            else
+            {
+                gPattern.Clip = new Region(clippingPath);
+
+                gPattern.RotateTransform(-angle);
+                gPattern.TranslateTransform(-x, -y);
+                gPattern.DrawImage(originalImage, scaledImgRect, imgRect, GraphicsUnit.Pixel);
+                gPattern.Dispose();
+            }
 
             if (pictureBox != null)
             {
@@ -75,6 +112,56 @@ namespace Kvh.Kaleidoscope
             }
 
             return new KaleidoscopeTemplate(pattern, patternWidth, patternHeight);
+        }
+
+
+        // 
+        public static void ApplyAlphaMask(Bitmap bmp, Bitmap alphaMaskImage)
+        {
+            int width = bmp.Width;
+            int height = bmp.Height;
+
+            BitmapData dataAlphaMask = alphaMaskImage.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+            try
+            {
+                BitmapData data = bmp.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadWrite, PixelFormat.Format32bppArgb);
+                try
+                {
+                    unsafe //using pointer requires the unsafe keyword
+                    {
+                        byte* pData0Mask = (byte*)dataAlphaMask.Scan0;
+                        byte* pData0 = (byte*)data.Scan0;
+
+                        for (int x = 0; x < width; x++)
+                        {
+                            for (int y = 0; y < height; y++)
+                            {
+                                byte* pData = pData0 + (y * data.Stride) + (x * 4);
+                                byte* pDataMask = pData0Mask + (y * dataAlphaMask.Stride) + (x * 4);
+
+                                byte maskBlue = pDataMask[0];
+                                byte maskGreen = pDataMask[1];
+                                byte maskRed = pDataMask[2];
+
+                                //the closer the color is to black the more opaque it will be.
+                                byte alpha = (byte)(255 - (maskRed + maskBlue + maskGreen) / 3);
+
+                                //respect the original alpha value
+                                byte originalAlpha = pData[3];
+                                pData[3] = (byte)(alpha * originalAlpha / 255f);
+                            }
+                        }
+                    }
+                }
+                finally
+                {
+                    bmp.UnlockBits(data);
+                }
+            }
+            finally
+            {
+                alphaMaskImage.UnlockBits(dataAlphaMask);
+            }
         }
     }
 }
