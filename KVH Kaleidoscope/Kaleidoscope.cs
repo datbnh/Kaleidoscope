0fc22733616855f5acadba419990d5eb4198d4b5
Dat Bui <datbnh@hotmail.com>
2020-04-22 20:39:20 +1000

    Code refactoring and cleaning up.

--- KVH Kaleidoscope/Kaleidoscope.cs
+++ KVH Kaleidoscope/Kaleidoscope.cs
@@ -6,6 +6,13 @@ namespace Kvh.Kaleidoscope
 {
     public static class Kaleidoscope
     {
+        /// <summary>
+        /// Renders image as a bitmap with specified size using specified pattern.
+        /// </summary>
+        /// <param name="width">Width of the output bitmap</param>
+        /// <param name="height">Height of the output bitmap</param>
+        /// <param name="pattern">Input pattern</param>
+        /// <returns></returns>
         public static Bitmap Render(int width, int height, KaleidoscopeTemplate pattern)
         {
             if (pattern == null)
@@ -57,12 +64,15 @@ namespace Kvh.Kaleidoscope
             return bitmap;
         }
 
+        
+        /// <summary>
+        /// Fills all pixels that have alpha channel value less than 255 by averaging neighbour pixels.
+        /// </summary>
+        /// <param name="bmp"></param>
         public static void FillVoid(Bitmap bmp)
         {
-            int[] xIdx = new int[] { };
-            int[] yIdx = new int[] { };
-            int r = 0, g = 0, b = 0;
-
+            _ = new int[] { };
+            _ = new int[] { };
             for (int x = 0; x < bmp.Width; x++)
             {
                 for (int y = 0; y < bmp.Height; y++)
@@ -70,74 +80,90 @@ namespace Kvh.Kaleidoscope
                     Color clr = bmp.GetPixel(x, y);
                     if (clr.A < 255)
                     {
-                        r = 0;
-                        g = 0;
-                        b = 0;
-                        if (x == 0)
-                        {
-                            if (y == 0)
-                            {
-                                xIdx = new int[] { 0, 1, 1 };
-                                yIdx = new int[] { 1, 0, 1 };
-                            }
-                            else if (y == bmp.Height - 1)
-                            {
-                                xIdx = new int[] { 0, 1, 1 };
-                                yIdx = new int[] { -1, -1, 0 };
-                            }
-                            else
-                            {
-                                xIdx = new int[] { 0, 0, 1, 1, 1 };
-                                yIdx = new int[] { -1, 1, -1, 0, 1 };
-                            }
-                        }
-                        else if (x == bmp.Width - 1)
-                        {
-                            if (y == 0)
-                            {
-                                xIdx = new int[] { -1, -1, 0 };
-                                yIdx = new int[] { 0, 1, 1 };
-                            }
-                            else if (y == bmp.Height - 1)
-                            {
-                                xIdx = new int[] { -1, -1, 0 };
-                                yIdx = new int[] { -1, 0, -1 };
-                            }
-                            else
-                            {
-                                xIdx = new int[] { -1, -1, -1, 0, 0 };
-                                yIdx = new int[] { -1, 0, 1, -1, 1, };
-                            }
-                        }
-                        else
-                        {
-                            if (y == 0)
-                            {
-                                xIdx = new int[] { -1, -1, 0, 1, 1 };
-                                yIdx = new int[] { 0, 1, 1, 0, 1 };
-                            }
-                            else if (y == bmp.Height - 1)
-                            {
-                                xIdx = new int[] { -1, -1, 0, 1, 1 };
-                                yIdx = new int[] { -1, 0, -1, -1, 0 };
-                            }
-                            else
-                            {
-                                xIdx = new int[] { -1, -1, -1, 0, 0, 1, 1, 1 };
-                                yIdx = new int[] { -1, 0, 1, -1, 1, -1, 0, 1 };
-                            }
-                        }
+                        int r = 0;
+                        int g = 0;
+                        int b = 0;
+                        GetCoordinateOfNeighbouringPixels(bmp, x, y, out int[] xIdx, out int[] yIdx);
                         for (var i = 0; i < xIdx.Length; i++)
                         {
                             r += bmp.GetPixel(x + xIdx[i], y + yIdx[i]).R;
                             g += bmp.GetPixel(x + xIdx[i], y + yIdx[i]).G;
                             b += bmp.GetPixel(x + xIdx[i], y + yIdx[i]).B;
                         }
-                        r = r / xIdx.Length;
-                        g = g / xIdx.Length;
-                        b = b / xIdx.Length;
+                        r /= xIdx.Length;
+                        g /= xIdx.Length;
+                        b /= xIdx.Length;
                         bmp.SetPixel(x, y, Color.FromArgb(255, r, g, b));
                     }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns (x, y) coordinate 
+        /// of all neighbouring pixels of a pixel 
+        /// in the bitmap bmp 
+        /// specified by its (x, y) coordinate to xIdx and yIdx arrays.
+        /// </summary>
+        /// <param name="bmp"></param>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="xIdx"></param>
+        /// <param name="yIdx"></param>
+        private static void GetCoordinateOfNeighbouringPixels(Bitmap bmp, int x, int y, out int[] xIdx, out int[] yIdx)
+        {
+            if (x == 0)
+            {
+                if (y == 0)
+                {
+                    xIdx = new int[] { 0, 1, 1 };
+                    yIdx = new int[] { 1, 0, 1 };
+                }
+                else if (y == bmp.Height - 1)
+                {
+                    xIdx = new int[] { 0, 1, 1 };
+                    yIdx = new int[] { -1, -1, 0 };
+                }
+                else
+                {
+                    xIdx = new int[] { 0, 0, 1, 1, 1 };
+                    yIdx = new int[] { -1, 1, -1, 0, 1 };
+                }
+            }
+            else if (x == bmp.Width - 1)
+            {
+                if (y == 0)
+                {
+                    xIdx = new int[] { -1, -1, 0 };
+                    yIdx = new int[] { 0, 1, 1 };
+                }
+                else if (y == bmp.Height - 1)
+                {
+                    xIdx = new int[] { -1, -1, 0 };
+                    yIdx = new int[] { -1, 0, -1 };
+                }
+                else
+                {
+                    xIdx = new int[] { -1, -1, -1, 0, 0 };
+                    yIdx = new int[] { -1, 0, 1, -1, 1, };
+                }
+            }
+            else
+            {
+                if (y == 0)
+                {
+                    xIdx = new int[] { -1, -1, 0, 1, 1 };
+                    yIdx = new int[] { 0, 1, 1, 0, 1 };
+                }
+                else if (y == bmp.Height - 1)
+                {
+                    xIdx = new int[] { -1, -1, 0, 1, 1 };
+                    yIdx = new int[] { -1, 0, -1, -1, 0 };
+                }
+                else
+                {
+                    xIdx = new int[] { -1, -1, -1, 0, 0, 1, 1, 1 };
+                    yIdx = new int[] { -1, 0, 1, -1, 1, -1, 0, 1 };
                 }
             }
         }
@@ -160,6 +186,35 @@ namespace Kvh.Kaleidoscope
             g.TranslateTransform(-(nTotalCols - nLeft) * setWidth, 0);
         }
 
+        /// <summary>
+        /// <para>Lets graphic g draw a set of specified patterns various times at different angles and offset.</para>
+        /// <para>
+        /// <br>Length of xOffsetFactors, yOffsetFactors, rotationAngles and patternIndices must be the same.</br>
+        /// <br>This function will iterate xOffsetFactors.Length times,</br>
+        /// <br>in iteration i, patternIndices[i] is selected and drawn</br>
+        /// <br>at coordinate (xOffsetFactors[i]*patternWidth, xOffsetFactors[i]*patternWidth)</br>
+        /// <br>and at an angle rotationAngles[i].</br>
+        /// </para>
+        /// <para>For example, if specified parameters are as below:
+        /// <br>. patterns = [image0, image1],</br>
+        /// <br>. xOffsetFactors = [1, 2, 3],</br>
+        /// <br>. yOffsetFactors = [11, 22, 33],</br>
+        /// <br>. rotationAngles = [angle1, angle2, angle3],</br>
+        /// <br>. patternIndices = [1, 0, 1],</br>
+        /// <br>The drawing sequence will be:</br>
+        /// <br>(1) draws image1 at (1*patternWidth, 11*patternHeight) with an angle of angle1,</br>
+        /// <br>(1) draws image0 at (2*patternWidth, 22*patternHeight) with an angle of angle2,</br>
+        /// <br>(1) draws image1 at (3*patternWidth, 33*patternHeight) with an angle of angle3.</br>
+        /// </para>
+        /// </summary>
+        /// <param name="g"></param>
+        /// <param name="patterns"></param>
+        /// <param name="patternWidth"></param>
+        /// <param name="patternHeight"></param>
+        /// <param name="xOffsetFactors"></param>
+        /// <param name="yOffsetFactors"></param>
+        /// <param name="rotationAngles"></param>
+        /// <param name="patternIndices"></param>
         private static void DrawSet(Graphics g, Bitmap[] patterns,
             float patternWidth, float patternHeight,
             float[] xOffsetFactors, float[] yOffsetFactors, float[] rotationAngles, int[] patternIndices)
@@ -176,12 +231,20 @@ namespace Kvh.Kaleidoscope
             }
         }
 
-        private static void DrawImageAtPointAndAngle(Graphics g, Bitmap bmp, float x, float y, float degree)
+        /// <summary>
+        /// Let grpahic g draw the specified bitmap at point (x, y) with an angle (degree).
+        /// </summary>
+        /// <param name="g"></param>
+        /// <param name="bmp"></param>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="degrees"></param>
+        private static void DrawImageAtPointAndAngle(Graphics g, Bitmap bmp, float x, float y, float degrees)
         {
             g.TranslateTransform(x, y);
-            g.RotateTransform(degree);
+            g.RotateTransform(degrees);
             g.DrawImage(bmp, 0, 0);
-            g.RotateTransform(-degree);
+            g.RotateTransform(-degrees);
             g.TranslateTransform(-x, -y);
         }
     }
